f746dda72e69b78d81e3cef7cce66cc93a1637e4
arachnid-cb <archana.sriram@careerbuilder.com>
2015-07-01 11:22:09 -0500

    test hashes, refactor

--- spec/lib/logfmtr_spec.rb
+++ spec/lib/logfmtr_spec.rb
@@ -8,11 +8,18 @@ describe Logfmtr::LogfmtLogger do
   end
 
   it "logs using default datetime format" do
-    expect { log(default_datetime_format) }.to output("level=ERROR datetime=#{Time.now.strftime(default_datetime_format)} progname= msg=some output\n").to_stdout
+    dt_format = default_datetime_format
+    expect { log_error(nil) }.to output("level=ERROR datetime=#{Time.now.strftime(dt_format)} progname= msg=some output\n").to_stdout
   end
 
   it "logs using custom datetime format" do
-    expect { log(custom_datetime_format) }.to output("level=ERROR datetime=#{Time.now.strftime(custom_datetime_format)} progname= msg=some output\n").to_stdout
+    dt_format = custom_datetime_format
+    expect { log_error(dt_format) }.to output("level=ERROR datetime=#{Time.now.strftime(dt_format)} progname= msg=some output\n").to_stdout
+  end
+
+  it "logs hashes smarter" do
+    dt_format = default_datetime_format
+    expect { log_hash(dt_format) }.to output("level=INFO datetime=#{Time.now.strftime(dt_format)} progname= one=1 two=2\n").to_stdout
   end
 end
 
@@ -24,12 +31,22 @@ def default_datetime_format
   "%Y-%m-%d %H:%M:%S %z"
 end
 
-def log(datetime_format)
+def log_error(datetime_format)
   begin
-    logger = Logger.new($stdout)
-    logger.formatter = Logfmtr::LogfmtLogger.new(datetime_format)
+    logger = logger_init(datetime_format)
     raise StandardError.new('some output')
   rescue => e
     logger.error e
   end
+end
+
+def log_hash(datetime_format)
+  logger = logger_init(datetime_format)
+  logger.info ({ one: 1, two: 2 })
+end
+
+def logger_init(datetime_format)
+  logger = Logger.new($stdout)
+  logger.formatter = Logfmtr::LogfmtLogger.new(datetime_format)
+  logger
 end
